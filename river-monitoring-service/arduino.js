0a58ea7dfcd3c198cb5af01110dbe57a1a87ad46
Pietro Ventrucci <pietroventrucci@gmail.com>
2024-03-20 15:03:52 +0100

    Simple example on how to use serial eith arduino

--- river-monitoring-service/arduino.js
+++ river-monitoring-service/arduino.js
@@ -1,6 +1,5 @@
 const { SerialPort } = require('serialport')
-const { ByteLengthParser } = require('@serialport/parser-byte-length')
-// const { ReadlineParser } = require('@serialport/parser-readline')
+const { ReadlineParser } = require('@serialport/parser-readline')
 
 const path = '/dev/ttyACM0'
 const baudRate = 115200
@@ -10,20 +9,62 @@ const port = new SerialPort({
     path: path,
     baudRate: baudRate,
 })
-// const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+
+const Modality = Object.freeze({
+    AUTO: 0,
+    MANUAL: 1,
+});
+
+const AlarmState = Object.freeze({
+    ALARM_TOO_LOW: 0,
+    NORMAL: 25,
+    PRE_ALARM_TOO_HIGH: 25,
+    ALARM_TOO_HIGH: 50,
+    ALARM_TOO_HIGH_CRITIC: 100,
+})
 
 
-// port.on('readable', function () {
-//     const data = port.read();
-//     console.log('Data:', data.toString())
-// })
+const valve_prefix = 'vl:'
+const mode_prefix = 'md:'
 
+function sendMessage(message) {
+    port.write(message, function (err) {
+        if (err) {
+            return console.log('Error on write: ', err.message)
+        }
+    })
+}
 
-// port.on('data', (data) => {
-//     console.log(data.toString());
-// });
+function parseData(data) {
+    if (data.startsWith(valve_prefix)) {
+        const valve = data.slice(valve_prefix.length)
+        console.log('Valve opening:', valve + '%')
+    }
+    else if (data.startsWith(mode_prefix)) {
+        const mode = data.slice(mode_prefix.length)
 
-const parser = port.pipe(new ByteLengthParser({ length: 1 }))
+        if (mode === Modality.MANUAL.toString()) {
+            console.log('Manual mode')
+
+        } else if (mode === Modality.AUTO.toString()) {
+            console.log('Automatic mode')
+        }
+
+    } else {
+        console.log(data)
+    }
+
+}
+
+// const parser = port.pipe(new ByteLengthParser({ length: 1 }))
 parser.on('data', function (data) {
-    console.log('Data:', data.toString())
+    parseData(data)
 }) // will have 8 bytes per data event
+
+
+// random from alarmstate
+setInterval(() => {
+    const rand = Math.floor(Math.random() * Object.keys(AlarmState).length)
+    sendMessage('vl:' + AlarmState[Object.keys(AlarmState)[rand]] + "\n")
+}, 10000)
