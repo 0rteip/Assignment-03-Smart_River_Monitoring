af71bfc19cf03eee668ea80857e8fd365d046b2c
Pietro Ventrucci <pietroventrucci@gmail.com>
2024-03-29 00:53:54 +0100

    Add possibility to change valve opening from dashboard

--- river-monitoring-dashboard/riciverAndShow.js
+++ river-monitoring-dashboard/riciverAndShow.js
@@ -1,39 +1,16 @@
 const ws = new WebSocket('ws://localhost:8080');
-// const messagesDiv = document.getElementById('messages');
-const modeP = document.getElementById('mode');
+const modeP = document.getElementById('state');
 const openingP = document.getElementById('opening');
-
-// Callback chiamata quando la connessione WebSocket viene stabilita
-ws.onopen = function () {
-    console.log('Connessione WebSocket stabilita');
-};
-
-// Callback chiamata quando il client riceve un messaggio WebSocket dal server
-ws.onmessage = function (event) {
-    const data = JSON.parse(event.data);
-    // Qui puoi fare qualsiasi cosa tu voglia con i dati ricevuti, ad esempio aggiornare l'interfaccia utente
-    if (data.topic === 'river-level') {
-        aggiornaInterfacciaUtente(data.message);
-    } else if (data.topic === 'current_state') {
-        modeP.innerHTML = data.description;
-        openingP.innerHTML = data.valveOpening;
-    } else {
-        console.log('Messaggio non gestito', data);
-    }
-};
+const valveButton = document.getElementById('valveButton');
+const valveInput = document.getElementById('openingInput');
 
 const xValues = [];
 const yValues = [];
 const maxElements = 10;
 
-function aggiornaInterfacciaUtente(message) {
-    // const l = document.createElement('li');
-    // l.innerHTML = message
-    // messagesDiv.appendChild(l);
-
-
-    xValues.push(Date.now());
-    yValues.push(parseFloat(message));
+function updateChart(newValue) {
+    xValues.push(new Date().toLocaleString());
+    yValues.push(parseFloat(newValue));
 
     if (xValues.length > maxElements) {
         xValues.shift();
@@ -56,7 +33,34 @@ function aggiornaInterfacciaUtente(message) {
     });
 }
 
-// Callback chiamata quando la connessione WebSocket viene chiusa
+ws.onopen = function () {
+    console.log('Connessione WebSocket stabilita');
+};
+
+ws.onmessage = function (event) {
+    const data = JSON.parse(event.data);
+
+    if (data.topic === 'river-level') {
+        updateChart(data.message);
+    } else if (data.topic === 'description') {
+        modeP.innerHTML = data.message;
+    } else if (data.topic === 'valve-opening') {
+        openingP.innerHTML = data.message;
+    } else {
+        console.log('Messaggio non gestito', data);
+    }
+};
+
 ws.onclose = function () {
     console.log('Connessione WebSocket chiusa');
 };
+
+valveButton.onclick = function () {
+    const opening = parseFloat(valveInput.value);
+    if (opening >= 0 && opening <= 100) {
+        ws.send(JSON.stringify({ topic: 'valve-opening', message: opening }));
+
+    } else {
+        alert('Inserire un valore compreso tra 0 e 100');
+    }
+}
--- river-monitoring-service/service.js
+++ river-monitoring-service/service.js
@@ -1,58 +1,39 @@
 const { SerialPort } = require('serialport')
 const mqtt = require('mqtt');
+const fs = require('fs');
 const WebSocket = require('ws');
 const { ReadlineParser } = require('@serialport/parser-readline')
 
-const path = '/dev/ttyACM0'
-const baudRate = 115200
-const wssPort = 8080
+const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+const {
+    Modality,
+    Frequecy,
+    AlarmState,
+    WaterLevel,
+    valve_prefix,
+    mode_prefix,
+    path,
+    baudRate,
+    wssPort } = data;
 
 const mqttClient = mqtt.connect('mqtt://broker.mqtt-dashboard.com');
 const wss = new WebSocket.Server({ port: wssPort });
 
 // Create a port
-const port = new SerialPort({
-    path: path,
-    baudRate: baudRate,
-})
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
-
-const Modality = Object.freeze({
-    AUTO: 0,
-    MANUAL: 1,
-});
-
-const Frequecy = Object.freeze({
-    F1: 5000,
-    F2: 1000
-})
-
-const AlarmState = Object.freeze({
-    ALARM_TOO_LOW: { description: "Alarm too low", valveOpening: 0, frequecy: Frequecy.F1 },
-    NORMAL: { description: "Normal", valveOpening: 25, frequecy: Frequecy.F1 },
-    PRE_ALARM_TOO_HIGH: { description: "Pre alarm too high", valveOpening: 25, frequecy: Frequecy.F2 },
-    ALARM_TOO_HIGH: { description: "Alarm too high", valveOpening: 50, frequecy: Frequecy.F2 },
-    ALARM_TOO_HIGH_CRITIC: { description: "Alarm too high critic", valveOpening: 100, frequecy: Frequecy.F2 },
-})
-
-const WaterLevel = Object.freeze({
-    WL1: 0.14,
-    WL2: 0.10,
-    WL3: 0.08,
-    WL4: 0.04
-})
-
-const valve_prefix = 'vl:'
-const mode_prefix = 'md:'
+// const port = new SerialPort({
+//     path: path,
+//     baudRate: baudRate,
+// })
+// const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
 let currentAlarmState = AlarmState.NORMAL
 
-function sendMessage(message) {
-    port.write(message, function (err) {
-        if (err) {
-            return console.log('Error on write: ', err.message)
-        }
-    })
+function sendSerialMessage(message) {
+    // port.write(message, function (err) {
+    //     if (err) {
+    //         return console.log('Error on write: ', err.message)
+    //     }
+    // })
 }
 
 function sendFrequency(frequency) {
@@ -62,7 +43,10 @@ function sendFrequency(frequency) {
 function sendWsData(topic, message) {
     wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ topic: topic, message: message.toString() }));
+            client.send(JSON.stringify({
+                topic: topic,
+                message: message
+            }));
         }
     });
 }
@@ -87,18 +71,13 @@ function parseData(data) {
     }
 }
 
-function sendMode(mode) {
-    if (currentAlarmState !== mode) {
+function sendMode(mode, force = false) {
+    if (currentAlarmState !== mode || force) {
         currentAlarmState = mode
-        sendMessage(valve_prefix + currentAlarmState.valveOpening + "\n")
+        sendSerialMessage(valve_prefix + currentAlarmState.valveOpening + "\n")
         sendFrequency(currentAlarmState.frequecy)
-        sendWsData('current_state', JSON.encode(
-            {
-                description: currentAlarmState.description,
-                valveOpening: currentAlarmState.valveOpening
-            }))
-        // sendWsData('alarm_state', currentAlarmState.description)
-        // sendWsData('opening', currentAlarmState.valveOpening)
+        sendWsData("description", currentAlarmState.description)
+        sendWsData("valve-opening", currentAlarmState.valveOpening)
     }
 }
 
@@ -116,10 +95,9 @@ function checkLevel(level) {
     }
 }
 
-// const parser = port.pipe(new ByteLengthParser({ length: 1 }))
-parser.on('data', function (data) {
-    parseData(data)
-}) // will have 8 bytes per data event
+// parser.on('data', function (data) {
+//     parseData(data)
+// })
 
 
 mqttClient.on('connect', function () {
@@ -136,7 +114,17 @@ mqttClient.on('message', function (topic, message) {
     }
 });
 
-
 wss.on('connection', function connection(ws) {
     console.log('Client connected');
+    sendMode(currentAlarmState, true);
+
+    ws.on('message', function incoming(message) {
+        const data = JSON.parse(message);
+        if (data.topic === 'valve-opening') {
+            sendSerialMessage(valve_prefix + data.message + "\n")
+            currentAlarmState.valveOpening = data.message
+            sendMode(currentAlarmState, true)
+        }
+
+    });
 });
